c892cdbfb3865b8876b2d06398098fad5f832f48
Belodubrovskii Daniil <dbelodubrovskij@yandex.ru>
2020-08-20 03:45:10 +0300

    Updates

---
+++ precache-manifest.fc29b9f0f5d90c0a1172add47062032c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22c6fb1deeaa05336af387ef4d206b88",
+    "url": "/authorization-react/index.html"
+  },
+  {
+    "revision": "08d795dd2049ca9fc1e0",
+    "url": "/authorization-react/static/css/main.032ad5a4.chunk.css"
+  },
+  {
+    "revision": "a0a3c9b977073837836f",
+    "url": "/authorization-react/static/js/2.36a943fe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/authorization-react/static/js/2.36a943fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08d795dd2049ca9fc1e0",
+    "url": "/authorization-react/static/js/main.bd4d96c8.chunk.js"
+  },
+  {
+    "revision": "5692a59825ad3466e1c6",
+    "url": "/authorization-react/static/js/runtime-main.a18c6ce5.js"
+  },
+  {
+    "revision": "c2d0901f14de58acae456bda23f80e8a",
+    "url": "/authorization-react/static/media/closeIcon.c2d0901f.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/authorization/precache-manifest.79a63dd054383738dead09dbd2e26402.js"
+  "/authorization-react/precache-manifest.fc29b9f0f5d90c0a1172add47062032c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/authorization/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/authorization-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
